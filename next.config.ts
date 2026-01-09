f4899a025d961a67a13a770fa0586f52785c1f18
Abdul kabir toyyib <kabirtoyyib19@gmail.com>
2026-01-09 20:44:42 +0000

    Invalid src prop (https://www.pngall.com/wp-content/uploads/5/Blue-Hat-P

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.pngall.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
